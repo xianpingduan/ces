de4eceaa0b4148fb11e7f9e2bdaad985d7fcea2e
allenduan <allenduan@pada.cc>
2014-12-03 20:38:44 +0800

    add start pushmsg service

--- src/com/xiexin/ces/activity/SplashActivity.java
+++ src/com/xiexin/ces/activity/SplashActivity.java
@@ -15,124 +15,156 @@ import com.xiexin.ces.Constants;
 import com.xiexin.ces.R;
 import com.xiexin.ces.utils.Logger;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends Activity
+{
 
-	public final static String TAG = "SplashActivity";
+    public final static String TAG = "SplashActivity";
 
-	private boolean mIsSetSerVerConfig = false;
+    private boolean mIsSetSerVerConfig = false;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
-		
-		//		test();
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+	// TODO Auto-generated method stub
+	super.onCreate( savedInstanceState );
+	setContentView( R.layout.activity_splash );
 
-		String url = App.getSharedPreference().getString(
-				Constants.SERVER_CONFIG_URL, "");
-		String port = App.getSharedPreference().getString(
-				Constants.SERVER_CONFIG_PORT, "");
-		Constants.ROOT = url + ":" + port;
-		Logger.d(TAG, "server url=" + url + ":" + port);
-		if (url.isEmpty()) {
-			mIsSetSerVerConfig = true;
+	//启动拉取服务
+	startPushService( );
+
+	//		test();
+
+	String url = App.getSharedPreference( ).getString( Constants.SERVER_CONFIG_URL , "" );
+	String port = App.getSharedPreference( ).getString( Constants.SERVER_CONFIG_PORT , "" );
+	Constants.ROOT = url + ":" + port;
+	Logger.d( TAG , "server url=" + url + ":" + port );
+	if( url.isEmpty( ) )
+	{
+	    mIsSetSerVerConfig = true;
+	}
+
+	new Handler( ).postDelayed( new Runnable( )
+	{
+	    @Override
+	    public void run()
+	    {
+		// 加载数据，跳转到登录界面
+		// TODO
+		if( mIsSetSerVerConfig )
+		{
+		    intentToServerConfig( );
 		}
-
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				// 加载数据，跳转到登录界面
-				// TODO
-				if (mIsSetSerVerConfig) {
-					intentToServerConfig();
-				} else {
-					doRequestServerConfigInfo();
-				}
-			}
-		}, 1000);
-	}
-
-	private final static int MSG_GET_SERVER_CONFOG_SUCCESS = 1;
-	private final static int MSG_GET_SERVER_CONFOG_ERROR = 2;
-	private Handler mUiHandler = new Handler() {
-
-		@Override
-		public void handleMessage(Message msg) {
-			// TODO Auto-generated method stub
-			super.handleMessage(msg);
-
-			switch (msg.what) {
-			case MSG_GET_SERVER_CONFOG_SUCCESS:
-				final boolean autoLogin = App.getSharedPreference().getBoolean(
-						Constants.AUTO_LOGIN, false);
-						if (autoLogin) {
-							intentToMain();
-						} else {
-							intentToLogin();
-						}
-						finish();
-				break;
-			case MSG_GET_SERVER_CONFOG_ERROR:
-				
-				break;
-			default:
-				break;
-			}
+		else
+		{
+		    doRequestServerConfigInfo( );
 		}
+	    }
+	} , 1000 );
+    }
 
-	};
-
-	// 加载服务器信息
-	private void doRequestServerConfigInfo() {
-		mUiHandler.sendEmptyMessage(MSG_GET_SERVER_CONFOG_SUCCESS);
-	}
-
-	private void intentToLogin() {
-		Intent intent = new Intent();
-		intent.setClass(SplashActivity.this, LoginActivity.class);
-		startActivity(intent);
-		finish();
-	}
-
-	private void intentToServerConfig() {
-		Intent intent = new Intent();
-		intent.setClass(SplashActivity.this, ServerConfigActivity.class);
-		startActivity(intent);
-		finish();
-	}
-
-	private void intentToMain() {
-		Intent intent = new Intent();
-		intent.setClass(SplashActivity.this, MenuActivity.class);
-		startActivity(intent);
-		finish();
-	}
+    private final static int MSG_GET_SERVER_CONFOG_SUCCESS = 1;
+    private final static int MSG_GET_SERVER_CONFOG_ERROR = 2;
+    private Handler mUiHandler = new Handler( )
+    {
 
 	@Override
-	protected void onResume() {
-		super.onResume();
+	public void handleMessage( Message msg )
+	{
+	    // TODO Auto-generated method stub
+	    super.handleMessage( msg );
+
+	    switch ( msg.what )
+	    {
+		case MSG_GET_SERVER_CONFOG_SUCCESS :
+		    final boolean autoLogin = App.getSharedPreference( ).getBoolean( Constants.AUTO_LOGIN , false );
+		    if( autoLogin )
+		    {
+			intentToMain( );
+		    }
+		    else
+		    {
+			intentToLogin( );
+		    }
+		    finish( );
+		    break;
+		case MSG_GET_SERVER_CONFOG_ERROR :
+
+		    break;
+		default :
+		    break;
+	    }
 	}
 
-	@Override
-	protected void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-	}
+    };
 
-	@Override
-	public void onBackPressed() {
-		return;
+    // 加载服务器信息
+    private void doRequestServerConfigInfo()
+    {
+	mUiHandler.sendEmptyMessage( MSG_GET_SERVER_CONFOG_SUCCESS );
+    }
+
+    private void intentToLogin()
+    {
+	Intent intent = new Intent( );
+	intent.setClass( SplashActivity.this , LoginActivity.class );
+	startActivity( intent );
+	finish( );
+    }
+
+    private void intentToServerConfig()
+    {
+	Intent intent = new Intent( );
+	intent.setClass( SplashActivity.this , ServerConfigActivity.class );
+	startActivity( intent );
+	finish( );
+    }
+
+    private void intentToMain()
+    {
+	Intent intent = new Intent( );
+	intent.setClass( SplashActivity.this , MenuActivity.class );
+	startActivity( intent );
+	finish( );
+    }
+
+    @Override
+    protected void onResume()
+    {
+	super.onResume( );
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+	// TODO Auto-generated method stub
+	super.onDestroy( );
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+	return;
+    }
+
+    private void test()
+    {
+
+	try
+	{
+	    JSONObject obj = new JSONObject( Constants.RQ001 );
+	    Log.d( TAG , obj.get( "Data" ).toString( ) );
 	}
-	
-	private void test(){
-		
-		try {
-			JSONObject  obj=new JSONObject(Constants.RQ001);
-			Log.d(TAG, obj.get("Data").toString());
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	catch ( JSONException e )
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace( );
 	}
+    }
+
+    private void startPushService()
+    {
+	Intent intent = new Intent( );
+	intent.setAction( "com.xiexin.ces.receiver.PushStartServiceReceiver" );
+	sendStickyBroadcast( intent );
+    }
 }
